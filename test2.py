8ae912468d39538dc508a5b980cec3731cd028d6
이태윤 <58839422+Lee-Tae-Yun@users.noreply.github.com>
2022-03-14 13:08:55 +0900

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,12 @@
+
+sum = 0
+for i in range(2,11,2):
+    sum += i
+    print(i,"+",sum-i,"=",sum)
+print("total =",sum)
+
+data = '안녕 '+\
+        '하세요 '+\
+        "파이썬 입니다."
+print(data)
+
---
+++ turtleTest.py
@@ -0,0 +1,9 @@
+import turtle
+
+turtle.shape('turtle')
+for i in range(400):
+    turtle.forward(10)
+    turtle.right(10)
+
+turtle.done()
+
