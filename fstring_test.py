11a704454f6d0255799a9e0f0d7c01d1e6214d02
이태윤 <58839422+Lee-Tae-Yun@users.noreply.github.com>
2022-04-04 13:58:29 +0900

    Add files via upload

---
+++ fstring_test.py
@@ -0,0 +1,4 @@
+for x in range(2,10):
+    for y in range(8,0,-2):
+        print (f'{x}*{y}={x*y}')
+    print("---------")
---
+++ list_Test.py
@@ -0,0 +1,15 @@
+#
+# l = ['apple','banana','mango']
+#
+# print(len(l))
+# print(l[0])
+# l.append("melon")
+# print(l.pop())
+# print(l.pop())
+#
+# print("-----------")
+# for i in l:
+#     print(i)
+
+for i in range(2,-3,-1):
+    print(i)
