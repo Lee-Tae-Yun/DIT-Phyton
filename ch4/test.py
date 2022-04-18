cc8ac76540a74f7f1a98f5e781706f7e1fe408f7
이태윤 <58839422+Lee-Tae-Yun@users.noreply.github.com>
2022-04-18 11:47:51 +0900

    Add files via upload

---
+++ ch4/test.py
@@ -0,0 +1,26 @@
+a = 'happy'
+b = 'love'
+print(f'a = {a} b = {b}')
+print('a = {0} b = {1}'.format(a,b))
+print('a = {key} b = {key2}'.format(key = a,key2 = b))
+
+
+s1 = '안안'\
+        '진진'\
+        '혁혁'
+print(s1)
+
+print("hello")
+print('/hello')
+print("I don't know")
+print('I dont\t know')
+print(r'c:/usr/bin')
+print("""\
+line1
+line2
+line3\
+""")
+
+print("""진진진진진진진진진진진진진진진
+        혁혁혁혁혁혁혁혁혁혁혁혁혁
+""")
